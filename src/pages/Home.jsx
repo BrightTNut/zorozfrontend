78fbf2d5df66b3aecc6a222b985ed226c953d0be
BrightTNut <118769123+BrightTNut@users.noreply.github.com>
2024-11-04 16:41:48 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     const fetchProducts = async () => {
       try {
         const response = await fetch(
-          "https://zoroz-ewuf.onrender.com//v1/allProducts"
+          "https://zoroz-ewuf.onrender.com/v1/allProducts"
         );
         const data = await response.json();
         setProducts(data);
